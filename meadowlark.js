753af185e49d9ea8926f0fe4b1b03aab8df62598
big_bear <xiongwei01@corp.netease.com>
2019-05-31 18:14:47 +0800

    5.11

--- meadowlark.js
+++ meadowlark.js
@@ -15,33 +15,41 @@ app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && 
         req.query.test === '1';
     next();
-})
+});
 
 app.get('/', function (req, res) {
     res.render('home'); 
-})
+});
 
 app.get('/about', function (req, res) {    
     res.render('about', {
         fortune: require('./lib/fortune')(),
         pageTestScript: '/qa/tests-about.js'
     });
-})
+});
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
 
 // 404 catch-all 处理器（中间件）
 app.use(function (req, res) {
     res.status(404);
     res.render('404'); 
-})
+});
 
 // 500 错误处理器（中间件）
 app.use(function (err, req, res, next) {
     console.log(err.stack);
     res.status(500);
     res.render('500'); 
-})
+});
 
 
 var server = app.listen(app.get('port'), function () {
     console.log('express started on  http://10.255.204.109:' + app.get('port') , server.address());
-})
+});
